1e93a01cccdb22a0b65fbb0fc770f382b3e86bdb
Eric Zhang <ezhang887@gmail.com>
2017-12-14 21:43:56 -0800

    restructure unit tests + add translation test class

---
+++ src/test/java/frc/team3256/lib/math/TranslationTest.java
@@ -0,0 +1,15 @@
+package frc.team3256.lib.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TranslationTest {
+
+    private final double kEpsilon = 1E-9;
+
+    @Test
+    public void testTranslation(){
+
+    }
+}
--- src/test/java/frc/team3256/lib/math/TwistTest.java
+++ src/test/java/frc/team3256/lib/math/TwistTest.java
@@ -8,9 +8,8 @@ public class TwistTest extends TestCase {
     private final double kEpsilon = 1E-9;
 
     @Test
-    public void testTwist(){
+    public void testConstructor(){
         Twist twist = new Twist();
-        //Constructor
         assertEquals(twist.dx(), 0, kEpsilon);
         assertEquals(twist.dy(), 0, kEpsilon);
         assertEquals(twist.dtheta(), 0, kEpsilon);
@@ -18,19 +17,23 @@ public class TwistTest extends TestCase {
         assertEquals(twist.dx(), 10, kEpsilon);
         assertEquals(twist.dy(), 1, kEpsilon);
         assertEquals(twist.dtheta(), 5, kEpsilon);
+    }
 
-        //scale
+    @Test
+    public void testScale(){
+        Twist twist = new Twist(10.0, 1.0, 5.0);
         twist = twist.scale(2.0);
         assertEquals(twist.dx(), 20.0, kEpsilon);
         assertEquals(twist.dy(), 2.0, kEpsilon);
         assertEquals(twist.dtheta(), 10.0, kEpsilon);
+    }
 
-        //exp
-        twist = new Twist(1, 0, 0);
+    @Test
+    public void testExp(){
+        Twist twist = new Twist(1, 0, 0);
         RigidTransform pose = RigidTransform.exp(twist);
         assertEquals(pose.getTranslation().x(), 1.0);
         assertEquals(pose.getTranslation().y(), 0.0);
         assertEquals(pose.getRotation().radians(), 0.0);
     }
-
 }

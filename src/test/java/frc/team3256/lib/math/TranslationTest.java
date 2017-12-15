fb694609f3cfea1ab62abff39d8b2049b25d0460
Eric Zhang <ezhang887@gmail.com>
2017-12-14 21:51:54 -0800

    add translation test base

--- src/test/java/frc/team3256/lib/math/TranslationTest.java
+++ src/test/java/frc/team3256/lib/math/TranslationTest.java
@@ -9,7 +9,51 @@ public class TranslationTest {
     private final double kEpsilon = 1E-9;
 
     @Test
-    public void testTranslation(){
+    public void testConstructor(){
+    }
+
+    @Test
+    public void testTranslate(){
+
+    }
+
+    @Test
+    public void testRotate(){
+
+    }
+
+    @Test
+    public void testInverse(){
+
+    }
+
+    @Test
+    public void testNorm(){
+
+    }
+
+    @Test
+    public void testDotProduct(){
+
+    }
+
+    @Test
+    public void testCrossProduct(){
+
+    }
+
+    @Test
+    public void testScale(){
+
+    }
+
+    @Test
+    public void testDirection(){
+
+    }
+
+    @Test
+    public void testAngleBetween(){
 
     }
 }

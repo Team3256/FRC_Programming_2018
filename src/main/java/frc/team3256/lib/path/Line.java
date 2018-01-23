4d394453728f0c96834fd75294b140b855ffc897
Bo887 <ezhang887@gmail.com>
2018-01-22 18:15:21 -0800

    Added comments for line segment ~Nic

--- src/main/java/frc/team3256/lib/path/Line.java
+++ src/main/java/frc/team3256/lib/path/Line.java
@@ -6,6 +6,12 @@ public class Line extends Segment{
 
     private Translation slope;
 
+    /**
+     * @param startX X-Coordinate of the starting position
+     * @param startY Y-Coordinate of the starting position
+     * @param endX X-Coordinate of the ending position
+     * @param endY Y-Coordinate of the ending position
+     */
     public Line(double startX, double startY, double endX, double endY){
         type = Type.LINE;
         start = new Translation(startX, startY);
@@ -13,30 +19,50 @@ public class Line extends Segment{
         slope = new Translation(start, end);
     }
 
+    /**
+     * @return type of the segment, in this case a line
+     */
     @Override
     public Type getType() {
         return type;
     }
 
+    /**
+     * @return start position of line
+     */
     @Override
     public Translation getStartPoint() {
         return start;
     }
 
+    /**
+     * @return end position of line
+     */
     @Override
     public Translation getEndPoint() {
         return end;
     }
 
+    /**
+     * @return slope of line
+     */
     public Translation getSlope() {
         return slope;
     }
 
+    /**
+     * @return length of line, .norm is pythagorean theorem
+     */
     @Override
-    public double getLength() {
-        return slope.norm();
-    }
+    public double getLength() { return slope.norm(); }
 
+    /**
+     * dot = dot product of u & v
+     * interpolationParameter = result of proj u onto v equation
+     *
+     * @param position current position of robot
+     * @return if proj u onto v < 0, start pos of line | if proj u onto v > 1, end pos of line | otherwise scale vector to lookahead distance
+     */
     @Override
     public Translation getClosestPointOnSegment(Translation position) {
         //Projection of the position onto the line
@@ -51,22 +77,39 @@ public class Line extends Segment{
         return start.translate(slope.scale(interpolationParameter));
     }
 
+    /**
+     * Finds our lookahead point
+     *
+     * @param lookaheadDistance distance of how far to look ahead of robot
+     * @param position current position of robot
+     * @return lookahead point
+     */
     @Override
     public Translation getLookAheadPoint(double lookaheadDistance, Translation position) {
+        //If our lookahead distance is higher than the line length, make the distance the line length
         if (lookaheadDistance > getLength()){
             lookaheadDistance = getLength();
         }
         Translation closestPoint = getClosestPointOnSegment(position);
+        //scales based on (currDist + lookaheadDist) / line length
         Translation scaledSlope = slope.scale((lookaheadDistance+getCurrDistanceTraveled(closestPoint))/(slope.norm()));
         return start.translate(scaledSlope);
     }
 
+    /**
+     * @param closestPoint closest point
+     * @return current distance traveled
+     */
     @Override
     public double getCurrDistanceTraveled(Translation closestPoint) {
         Translation startToClosestPoint = new Translation(start, closestPoint);
         return startToClosestPoint.norm();
     }
 
+    /**
+     * @param closestPoint closest point
+     * @return remaining distance based of current distance traveled
+     */
     @Override
     public double getRemainingDistance(Translation closestPoint){
         return getLength() - getCurrDistanceTraveled(closestPoint);
